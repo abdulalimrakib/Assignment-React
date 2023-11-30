f161ee2a1a51ba171b334efdc41b0d2a6a5af20f
abdulalimrakib <abdulalimrakib04@gmail.com>
2023-11-30 23:47:21 +0600

    stste lifting practice

---
+++ react-assignment-3-create-new-resource/src/components/NewTodo.jsx
@@ -0,0 +1,74 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { v4 as uuidv4 } from 'uuid';
+
+const NewTodo = ({handleAddTodo}) => {
+
+    const [todo, setTodo] = useState({title:"",id:"", disc: ""});
+
+    // for handling todo state changes
+    const handleChange = (e) => { 
+        setTodo({...todo,[e.target.name]: e.target.value})
+    };
+
+    // submit the form and send newTodo in App.js
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const newTodo = {
+            id: uuidv4(),
+            ...todo
+        };
+        
+        handleAddTodo(newTodo)
+
+        // for reset todo state
+        setTodo({
+            title: '',
+            desc: ''
+        });
+    };
+
+    return (
+        <div>
+            <h1 className="title">Collect Data from a Form</h1>
+            <div className="card">
+                <form action="" onSubmit={handleSubmit}>
+                    <div className="field-group">
+                        <label htmlFor="title">Title: </label>
+                        <input
+                            type="text"
+                            name="title"
+                            id="title"
+                            value={todo.title}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+
+                    <div className="field-group">
+                        <label htmlFor="desc">Description: </label>
+                        <textarea
+                            name="desc"
+                            id="desc"
+                            value={todo.desc}
+                            onChange={handleChange}
+                            required></textarea>
+                    </div>
+                    <div className="field-group">
+                        <button className="btn" type="submit">
+                            Send
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+NewTodo.propTypes = {
+    onHandleAddTodo: PropTypes.func
+};
+
+export default NewTodo
---
+++ react-assignment-3-create-new-resource/src/components/Todo.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Todo = (props) => {
+  const { title, desc } = props.todo;
+  return (
+    <article className="todo">
+      <h3 className="todo__title">{title}</h3>
+      <p>{desc}</p>
+    </article>
+  );
+};
+
+Todo.propTypes = {
+  todo: PropTypes.shape({
+    title: PropTypes.string,
+    desc: PropTypes.string
+  })
+};
+
+export default Todo
